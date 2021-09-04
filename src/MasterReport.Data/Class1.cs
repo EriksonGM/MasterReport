a40c25b3acb8c90e3fb22519b0f944b2458d23ba
Erikson Melgarejo <eriksonlove@gmail.com>
2021-09-04 15:17:25 +0100

    Add project files.

---
+++ src/MasterReport.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MasterReport.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/MasterReport.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MasterReport.Models
+{
+    public class Class1
+    {
+    }
+}
